546bdc63b3565f604c6e409fa6cee77aaa539d8d
Jon Chambers <jon.chambers@gmail.com>
2020-01-28 11:40:27 -0500

    Added a service for determining if users are mutual contacts.

--- src/main/java/com/eatthepath/signal/chat/ChatServerApplication.java
+++ src/main/java/com/eatthepath/signal/chat/ChatServerApplication.java
@@ -1,5 +1,7 @@
 package com.eatthepath.signal.chat;
 
+import com.eatthepath.signal.chat.contacts.ContactService;
+import com.eatthepath.signal.chat.contacts.InMemoryContactService;
 import com.eatthepath.signal.chat.http.HttpServer;
 
 import java.io.IOException;
@@ -7,6 +9,8 @@ import java.io.IOException;
 public class ChatServerApplication {
 
     public static void main(final String... args) throws IOException {
+        final ContactService contactService = new InMemoryContactService();
+
         final HttpServer httpServer = new HttpServer(80);
         httpServer.start();
     }
